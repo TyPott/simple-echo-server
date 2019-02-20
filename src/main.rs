a4dff90edcd4f0e01d4a0e2057a4b906a3375b16
Tyler Pottenger <tyler.pottenger@gmail.com>
2019-02-20 00:07:13 -0500

    Replace write with write_all

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,19 @@ use std::time::Duration;
 const HOST: &str = "127.0.0.1:8080";
 const BUF_SIZE: usize = 8 * 1024;
 
-fn echo<T: Read + Write>(client: &mut T) -> io::Result<usize> {
+fn echo<T: Read + Write>(client: &mut T) -> io::Result<()> {
     let mut buffer = [0; BUF_SIZE];
-    let mut written = 0;
     loop {
         let n = client.read(&mut buffer)?;
         if n == 0 {
             break;
         }
-        written += client.write(&buffer[0..n])?;
+        client.write_all(&buffer[0..n])?;
     }
-    Ok(written)
+    Ok(())
 }
 
-fn handle_client(stream: &mut TcpStream) -> io::Result<usize> {
+fn handle_client(stream: &mut TcpStream) -> io::Result<()> {
     stream.set_read_timeout(Some(Duration::from_secs(15)))?;
     stream.set_write_timeout(Some(Duration::from_secs(15)))?;
     echo(stream)
@@ -29,9 +28,8 @@ fn main() {
     println!("Server listening on {}", HOST);
 
     for client in listener.incoming() {
-        match client.and_then(|mut s| handle_client(&mut s)) {
-            Ok(n) => println!("Echoed {} bytes", n),
-            Err(e) => println!("Error reading/writing stream: {}", e),
+        if let Err(e) = client.and_then(|mut s| handle_client(&mut s)) {
+            println!("Error reading/writing stream: {}", e);
         }
     }
 }
@@ -45,7 +43,7 @@ mod tests {
     fn echo_in_memory_buffer() {
         let input = "Hello, world!";
         let mut buffer = Cursor::new(input.to_owned().into_bytes());
-        assert_eq!(input.len(), echo(&mut buffer).unwrap());
+        assert_eq!((), echo(&mut buffer).unwrap());
         assert_eq!(input.repeat(2).as_bytes(), buffer.get_ref().as_slice());
     }
 }
